c2a241a304f8f0c2497742ebbea25db6ae870e91
Bel Ortega <belenortega369@gmail.com>
2024-07-10 11:25:59 +0200

    Create visual component and style it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,17 @@
 <script>
-  import Header from './components/header.svelte';
+  import Header from '$lib/header.svelte';
+  import Visual from '$lib/visual.svelte';
 </script>
 
-<Header />
+<header>
+  <Header />
+</header>
+
+<body>
+  <Visual
+    title="Lorem ipsum dolor sit amet"
+    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rhoncus, at massa eleifend purus sagittis sed justo, purus diam."
+    button1Text="Lorem ipsum"
+    button2Text="Lorem ipum"
+  />
+</body>
